0b4bbb303b4b1f455f64a05c2df3ddd537abc8cb
Luiz Henrique <luizhf42@gmail.com>
2022-07-19 16:02:27 -0300

    fix: fix the image path bug again

--- components/SocialMedia.vue
+++ components/SocialMedia.vue
@@ -5,7 +5,7 @@
     rel="noopener noreferrer"
     class="wrapper discord"
   >
-    <img :src="svgPath" />
+    <img :src="`./assets/images/${socialMediaName.toLowerCase()}.svg`" />
     <h3>{{ socialMediaName }}</h3>
   </a>
 </template>
