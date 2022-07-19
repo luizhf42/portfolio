6959864280ecbe6f88971e76278cc9eff04724d9
Luiz Henrique <luizhf42@gmail.com>
2022-07-19 15:32:21 -0300

    feat: add the SocialMedia component

---
+++ components/SocialMedia.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    :href="socialMediaHref"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="wrapper discord"
+  >
+    <img :src="svgPath" />
+    <h3>{{ socialMediaName }}</h3>
+  </a>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  socialMediaName: String,
+  socialMediaHref: String,
+  svgPath: String,
+});
+</script>
+
+<style lang="postcss" scoped>
+.wrapper {
+  @apply bg-contentWrapper text-white flex items-center justify-center gap-3 h-24 px-6 py-4 rounded-xl transition hover:border hover:scale-105;
+}
+
+.discord {
+  @apply hover:border-[#5865F2] hover:border;
+}
+.instagram {
+  @apply hover:border-[#9C27B0] hover:border;
+}
+.linkedin {
+  @apply hover:border-[#0A66C2] hover:border;
+}
+.github {
+  @apply hover:border-white hover:border;
+}
+
+img {
+  @apply h-[85%] md:h-full;
+}
+
+h3 {
+  @apply text-3xl;
+}
+</style>
