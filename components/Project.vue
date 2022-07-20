a480bee1d69a4ef964a9411fd91a82d1f48f393e
Luiz Henrique <luizhf42@gmail.com>
2022-07-20 15:07:37 -0300

    feat: add Project component

---
+++ components/Project.vue
@@ -0,0 +1,43 @@
+<template>
+  <li class="project">
+    <div class="emoji">{{ emoji }}</div>
+    <div class="text">
+      <h3>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          :href="`https://github.com/luizhf42/${repositoryName}`"
+          >{{ name }}</a
+        >
+      </h3>
+      <p>{{ description }}</p>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+const projectProps = defineProps({
+  name: String,
+  emoji: String,
+  description: String,
+  repositoryName: String,
+});
+</script>
+
+<style lang="postcss" scoped>
+.project {
+  @apply w-full bg-contentWrapper flex items-center mb-4 h-16 sm:h-24 rounded-xl overflow-hidden text-white;
+}
+
+.emoji {
+  @apply h-full aspect-square flex items-center justify-center text-4xl sm:text-5xl;
+}
+
+h3 a {
+  @apply text-3xl font-bold xs:text-2xl sm:text-2xl hover:text-accent transition;
+}
+
+p {
+  @apply hidden text-sm sm:block;
+}
+</style>
