5db33659cbcb538337036b64cca6839409ad5676
Luiz Henrique <luizhf42@gmail.com>
2022-07-20 15:34:43 -0300

    feat: start the Projects page development

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,24 @@
 <template>
   <NuxtLayout name="main">
-    <p class="text-white">Not finished yet! Come back later.</p>
+    <p class="text-white mb-8">Not finished yet! Come back later.</p>
+    <section class="projects">
+      <ul>
+        <Project
+          v-for="project in projects"
+          :key="project.name"
+          :name="project.name"
+          :emoji="project.emoji"
+          :description="project.description"
+          :repositoryName="project.repositoryName"
+        />
+      </ul>
+    </section>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import projects from "public/projects.json";
+</script>
+
+<style lang="postcss" scoped>
+</style>
